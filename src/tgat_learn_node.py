e5827e84d2a82828a86a24cdd9d248072e8d8059
MiaHenstridge <nongngocmai22@gmail.com>
2026-03-04 22:12:45 +0100

    created arg parser and logger

--- src/tgat_learn_node.py
+++ src/tgat_learn_node.py
@@ -94,5 +94,20 @@ TIME_DIM = args.time_dim
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('log/{}.log'.format(str(time.time())))
 
+fh = logging.FileHandler('log/{}.log'.format(str(time.time())))
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARN)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+logger.info(args)
+
+
+# load data and train val test split
