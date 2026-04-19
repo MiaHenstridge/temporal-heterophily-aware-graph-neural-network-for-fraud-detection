fc3275304cf1208ae057a945e7945b77e556a726
MiaHenstridge <nongngocmai22@gmail.com>
2026-04-19 13:11:04 +0200

    updated bash script

--- src/run_thegcn_experiments.sh
+++ src/run_thegcn_experiments.sh
@@ -9,45 +9,49 @@ time_dim=128
 neighbors=(5 10)
 durations=(0 730 365)
 layers=(2 3 4)
-feat_augment=false # Set to true or false
+feat_aug_options=(true false)
 
-# Logic to determine the flag string
-if [ "$feat_augment" = true ]; then
-    augment_flag="--feat_augment"
-else
-    augment_flag=""
-fi
+# Total runs calculation
+total_runs=$((${#feat_aug_options[@]} * ${#layers[@]} * ${#durations[@]} * ${#neighbors[@]}))
 
-total_runs=$((${#layers[@]} * ${#durations[@]} * ${#neighbors[@]}))
+# Initialize run counter at 0
+run_id=0
 
-# Initialize run counter
-run_id=18
+for feat_augment in "${feat_aug_options[@]}"; do
+    
+    # Logic to determine the flag string
+    if [ "$feat_augment" = true ]; then
+        augment_flag="--feat_augment"
+    else
+        augment_flag=""
+    fi
 
-for l in "${layers[@]}"; do
-    for d in "${durations[@]}"; do
-        for n in "${neighbors[@]}"; do
-            # Create the run prefix
-            prefix="run$run_id"
-            
-            echo "Running: $prefix ($((run_id + 1))/$total_runs) | layer=$l, duration=$d, neighbor=$n"
-            
-            # Pass the prefix as an argument to your script
-            python src/07_thegcn_tuning_with_sampler.py \
-                --prefix "$prefix" \
-                --lr "$lr" \
-                --bs "$bs" \
-                --n_epoch "$epochs" \
-                --loss focal \
-                --node_dim "$node_dim"\
-                --time_dim "$time_dim"\
-                --n_layer "$l" \
-                --duration "$d" \
-                --n_neighbor "$n" \
-                --early_stop_higher_better \
-                $augment_flag
-            
-            # Increment counter for the next iteration
-            ((run_id++))
+    for l in "${layers[@]}"; do
+        for d in "${durations[@]}"; do
+            for n in "${neighbors[@]}"; do
+                # Create the run prefix
+                prefix="run$run_id"
+                
+                echo "Running: $prefix ($((run_id + 1))/$total_runs) | aug=$feat_augment, layer=$l, duration=$d, neighbor=$n"
+                
+                # Execute python script
+                python src/07_thegcn_tuning_with_sampler.py \
+                    --prefix "$prefix" \
+                    --lr "$lr" \
+                    --bs "$bs" \
+                    --n_epoch "$epochs" \
+                    --loss focal \
+                    --node_dim "$node_dim" \
+                    --time_dim "$time_dim" \
+                    --n_layer "$l" \
+                    --duration "$d" \
+                    --n_neighbor "$n" \
+                    --early_stop_higher_better \
+                    $augment_flag
+                
+                # Increment counter
+                ((run_id++))
+            done
         done
     done
 done
